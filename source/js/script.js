7f9dc06cccff7323e10d381a397977379be9e725
DVNst <dvn-studio@mail.ru>
2018-02-18 20:30:31 +0300

    добавляет скрипт для кнопки

---
+++ source/js/script.js
@@ -0,0 +1,14 @@
+var main_nav_toggle = document.querySelector(".main-nav__toggle");
+var main_nav = document.querySelector(".main-nav__list");
+
+window.onload = function() {
+  main_nav_toggle.classList.remove("no-js");
+  main_nav_toggle.classList.remove("main-nav__toggle--close");
+  main_nav.classList.add("main-nav__list--hidden");
+};
+
+main_nav_toggle.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  main_nav_toggle.classList.toggle("main-nav__toggle--close");
+  main_nav.classList.toggle("main-nav__list--hidden");
+});
