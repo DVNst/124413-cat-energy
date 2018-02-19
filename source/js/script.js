80617ca1ca97510513ce2eb039520713e4d87765
DVNst <dvn-studio@mail.ru>
2018-02-19 21:12:14 +0300

    добавляет исправляения Б5 Б31 Д14 Д21 Д23

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,12 @@
 var main_nav_toggle = document.querySelector(".main-nav__toggle");
 var main_nav = document.querySelector(".main-nav__list");
 
-window.onload = function() {
-  main_nav_toggle.classList.remove("no-js");
+main_nav_toggle.classList.remove("no-js");
 
-  if(window.screen.width <= 320) {
-    main_nav_toggle.classList.remove("main-nav__toggle--close");
-    main_nav.classList.add("main-nav__list--hidden");
-  }
-};
+if(window.screen.width <= 767) {
+  main_nav_toggle.classList.remove("main-nav__toggle--close");
+  main_nav.classList.add("main-nav__list--hidden");
+}
 
 main_nav_toggle.addEventListener("click", function (evt) {
   evt.preventDefault();
